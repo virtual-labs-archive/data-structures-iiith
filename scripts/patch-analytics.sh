887913d09ec79770107a856aa1f7a5c938754978
r <raj@vlabs.ac.in>
2019-11-27 13:44:24 +0530

    Fixed the path for gtm-patcher.py

--- scripts/patch-analytics.sh
+++ scripts/patch-analytics.sh
@@ -3,12 +3,12 @@
 # 	echo $entry
 #   "./gtmPatcher.py" $entry
 # done
-for entry in "../src/lab"/*
+for entry in "lab"/*
 do
   if [ -d "${entry}" ] ; then
     for another_file in $entry/*".html"
     do
-  	"./gtm-patcher.py" $another_file
+  	"../scripts/gtm-patcher.py" $another_file
     done  
   fi
 done 
